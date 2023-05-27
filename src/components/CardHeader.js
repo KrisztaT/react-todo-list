3298f4546bae9fd4c29f481edb9c2807236aa3c5
KrisztaT <itsforadsmail@gmail.com>
2023-05-27 17:35:26 +1000

    fix: sort using dropdown, styling change in CardHeader

--- src/components/CardHeader.js
+++ src/components/CardHeader.js
@@ -8,6 +8,14 @@ const CardHeader = ({ dispatch }) => {
   const [weatherData, setWeatherData] = useState(null);
   const [lastFetchDate, setLastFetchDate] = useLocalStorage("lastFetchDate", 0);
 
+  const handleDropdownSelect = (eventKey) => {
+    if (eventKey === "dueDate") {
+      sortByDueDate();
+    } else if (eventKey === "status") {
+      sortByStatus();
+    }
+  };
+
   const sortByDueDate = () => {
     dispatch({ type: "SORT_BY_DUE_DATE" });
   };
@@ -59,29 +67,9 @@ const CardHeader = ({ dispatch }) => {
     <Card.Header className="text-center bg-white sticky-top">
       <p className="fs-1 text-secondary mb-0">ToDo List</p>
       <ListGroup className="overflow auto">
-        <ListGroup.Item>
+        <ListGroup.Item className="p-0 m-0">
           <div class="container">
-            <div class="row justify-content-between align-items-center">
-              <div class="col-auto">
-                <div className="d-flex justify-content-between align-items-center">
-                <Dropdown onSelect={sortByDueDate}>
-                  <Dropdown.Toggle
-                    variant="outline-secondary"
-                    id="sorting-dropdown"
-                  >
-                    Sort by
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item onSelect={sortByDueDate}>
-                    due date <SortDownAlt size={16} />
-                    </Dropdown.Item>
-                    <Dropdown.Item onSelect={sortByStatus}>
-                    status  <FunnelFill size={16} />
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-                </div>
-              </div>
+            <div class="row justify-content-evenly align-items-center">
               <div class="col-auto">
                 {weatherData && (
                   <div className="d-flex align-items-center">
@@ -98,6 +86,25 @@ const CardHeader = ({ dispatch }) => {
                     </p>
                   </div>
                 )}
+              </div>
+              <div class="col-auto">
+                <Dropdown onSelect={handleDropdownSelect}>
+                  <Dropdown.Toggle
+                    variant="outline-secondary"
+                    id="sorting-dropdown"
+                    className="btn-sm"
+                  >
+                    Sort by
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item eventKey="dueDate">
+                      Due date <SortDownAlt size={20} />
+                    </Dropdown.Item>
+                    <Dropdown.Item eventKey="status">
+                      Status <FunnelFill size={18} />
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
               </div>
             </div>
           </div>
